82450f8f05d3d33ab7e191594c0c5ca625cafe8a
fox <fernandodiego.sn@gmail.com>
2023-09-14 12:29:47 -0300

    Encontrar menor número em uma lista em python

---
+++ pythonEst00.py
@@ -0,0 +1,8 @@
+# Define the list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Find the minimum number in the list
+minimum_number = min(numbers)
+
+# Print the minimum number
+print("The minimum number in the list is:", minimum_number)
