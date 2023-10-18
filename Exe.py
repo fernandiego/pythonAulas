20eb6ab083d3604af90124cddd5c6fb3b19d4c5e
fox <fernandodiego.sn@gmail.com>
2023-10-18 19:23:28 -0300

    Algumas questões das provas que valem 2pts

---
+++ Exe.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/ola')
+def ola_mundo():
+    return "Olá,mundo"
+
+@app.route('/ola/<nome>')
+def hola_mundo(nome):
+    return "Olá, " + nome
+
+if __name__ == '__main__':
+    app.run()
